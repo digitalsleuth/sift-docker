867f1fbba04c28700c6e44cf923452f46fe9cee4
Digital Sleuth <62841822+digitalsleuth@users.noreply.github.com>
2020-04-01 22:13:12 -0400

    Correction to name parameter

--- runsift.sh
+++ runsift.sh
@@ -5,5 +5,5 @@
 # --privileged, --cap-add SYS_ADMIN and --cap-add MKNOD are required to provide the required permissions to mount fuse devices
 sudo docker network rm sift_net > /dev/null
 sudo docker network create --subnet=172.21.0.0/16 sift_net > /dev/null 
-sudo docker run -d -v <host_folder_to_share>:<docker_folder_to_map>:ro --net sift_net --hostname sift --ip 172.21.0.3 --name --privileged --cap-add SYS_ADMIN --cap-add MKNOD --device /dev/fuse sift digitalsleuth/sift-docker > /dev/null
+sudo docker run -d -v <host_folder_to_share>:<docker_folder_to_map>:ro --net sift_net --hostname sift --ip 172.21.0.3 --name sift --privileged --cap-add SYS_ADMIN --cap-add MKNOD --device /dev/fuse digitalsleuth/sift-docker > /dev/null
 sudo docker ps -q | sudo xargs -n 1 docker inspect --format "{{.Config.Image}} {{.Name}} {{range .NetworkSettings.Networks}} {{.IPAddress}}{{end}}"
